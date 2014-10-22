4e728cfcc51d32455365cac322bd0a4bbdd3f7e3
Ingolfur Edvardsson <ingolfured@gmail.com>
2014-10-22 21:10:41 +0100

    the only commit, see readme for explanation

---
+++ README.md
@@ -0,0 +1,11 @@
+This program should be pretty self explanable. However, in case F, I use
+a modification of BFS to traverse the matrix. When I finished I saw that
+you wanted several commits. Im sorry about that. I simply setup the when
+statements and wrote each case, starting from top to bottom. In the end
+I added the argument warning because it was so easy to forget exactly
+how the input should be.
+
+To run this program, simply run
+irb -r ./carwow.rb
+no instruction were written since we assume the user has the problem
+sheet given. 
---
+++ carwow.rb
@@ -0,0 +1,89 @@
+action = ''
+puts "Possible inputs: I,C,L,V,H,F and X"
+while action != 'X' do
+  input = gets.chomp.split
+  action = input[0]
+  case
+  when action == 'I'
+    if input.size == 3
+      pixels = Array.new(input[2].to_i){Array.new(input[1].to_i){'O'}}
+    else
+      puts "Wrong number of arguments, please try again"
+    end
+
+  when action == 'C'
+    if input.size == 1
+      pixels.each do |y|
+        y.each do |x|
+          x = 'O'
+        end
+      end
+    else
+      puts "Wrong number of arguments, please try again"
+    end
+
+
+  when action == 'L'
+    if input.size == 4
+      pixels[input[2].to_i-1][input[1].to_i-1] = input[3]
+    else
+      puts "Wrong number of arguments, please try again"
+    end
+
+  when action == 'H'
+    if input.size == 5
+      for i in input[1].to_i..input[2].to_i do
+        pixels[input[3].to_i-1][i-1] = input[4]
+      end
+    else
+      puts "Wrong number of arguments, please try again"
+    end
+
+  when action == 'V'
+    if input.size == 5
+      for i in input[2].to_i..input[3].to_i do
+        pixels[i-1][input[1].to_i-1] = input[4]
+      end
+    else
+      puts "Wrong number of arguments, please try again"
+    end
+
+  when action == 'F'
+    if input.size == 4
+      i1 = (input[2].to_i) -1
+      i2 = (input[1].to_i) -1
+      base_color =   pixels[i1][i2]
+      q = Queue.new
+      q << [i1,i2]
+
+      while not q.empty? do
+        v = q.pop
+        pixels[v[0]][v[1]] = input[3]
+        [ [v[0]-1,v[1]], [v[0]+1,v[1]], [v[0],v[1]+1], [v[0],v[1]-1] ].each do |j|
+          if 0 <= j[0] and 0<= j[1] and j[0] < pixels.length and j[1] < pixels[0].length  and  pixels[j[0]][j[1]] == base_color
+            pixels[j[0]][j[1]] = input[3]
+            q << j
+          end
+        end
+      end
+    else
+      puts "Wrong number of arguments, please try again"
+    end
+
+
+
+  when action == 'S'
+    if input.size == 1
+      pixels.each do |row|
+        row.each do |x|
+          print x
+        end
+        puts
+      end
+    else
+      puts "Wrong number of arguments, please try again"
+    end
+  else
+    puts "Illegal input, please try again"
+  end
+end
